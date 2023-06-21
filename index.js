963e8fbaa0445fa712632a136797269d5da89aae
Anileleye Awwal <aanileleye@gmail.com>
2023-06-21 10:40:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav_toggle')
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', function() {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        document.body.classList.remove('nav-open');
+    })
+})
